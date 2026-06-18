a0fb2353a8ae782840520f3e2aabe999fcc3abc5
taimv2405 <taimv2405@gmail.com>
2026-06-18 08:38:15 +0000

    [13] feat(patientor): add POST /api/patients endpoint
    Define Request body as NewPatient to avoid disabling ESLint rules.

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,5 +1,5 @@
-import express, { type Response } from 'express';
-import type { NonSensitivePatient } from '../types.ts';
+import express, { type Response, type Request } from 'express';
+import type { NonSensitivePatient, Patient, NewPatient } from '../types.ts';
 import patientService from '../services/patientService.ts';
 
 const router = express.Router();
@@ -7,5 +7,28 @@ const router = express.Router();
 router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.json(patientService.getAllNonSensitive());
 });
+
+router.post(
+  '/',
+  (
+    req: Request<unknown, unknown, NewPatient>,
+    res: Response<Patient | string>,
+  ) => {
+    const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+
+    if (!name || !dateOfBirth || !ssn || !gender || !occupation) {
+      return res.status(400).send('Missing required fields');
+    }
+
+    const createdPatient = patientService.createPatient({
+      name,
+      dateOfBirth,
+      ssn,
+      gender,
+      occupation,
+    });
+    return res.status(201).json(createdPatient);
+  },
+);
 
 export default router;
--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,7 +1,14 @@
 import patients from '../../data/patients.ts';
-import type { NonSensitivePatient } from '../types.ts';
+import type { NonSensitivePatient, NewPatient, Patient } from '../types.ts';
+import { v1 as uuid } from 'uuid';
 
 const getAllNonSensitive = (): NonSensitivePatient[] =>
   patients.map(({ ssn: _ssn, ...patient }) => ({ ...patient }));
 
-export default { getAllNonSensitive };
+const createPatient = (inputPatient: NewPatient): Patient => {
+  const newPatient = { ...inputPatient, id: uuid() };
+  patients.push(newPatient);
+  return newPatient;
+};
+
+export default { getAllNonSensitive, createPatient };
