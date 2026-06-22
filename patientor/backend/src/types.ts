b84d3b0a8bd0bfa1491bae17b61db63a501ae376
taimv2405 <taimv2405@gmail.com>
2026-06-22 08:44:56 +0000

    [25] feat(patientor): add entry types and seed patient entries

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -27,8 +27,54 @@ export const NewPatientSchema = z.object({
 
 export type NewPatient = z.infer<typeof NewPatientSchema>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+
+type HealthCheckRating =
+  (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient extends NewPatient {
   id: string;
